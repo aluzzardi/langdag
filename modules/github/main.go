ae692fa378505cc2ee790dd6245ba2b3d9098590
Andrea Luzzardi <andrea@luzzardi.com>
2025-01-21 16:21:38 -0800

    github module: issue create and close support
    Signed-off-by: Andrea Luzzardi <andrea@luzzardi.com>

--- modules/github/main.go
+++ modules/github/main.go
@@ -38,6 +38,55 @@ func (m *Github) base() *dagger.Container {
 		WithSecretVariable("GITHUB_TOKEN", m.Token)
 }
 
+// Create an issue on GitHub.
+func (m *Github) IssueCreate(
+	ctx context.Context,
+	// Select another repository using the [HOST/]OWNER/REPO format
+	repo string,
+
+	// Assign people by their login. Use "@me" to self-assign.
+	// +optional
+	assignee string,
+
+	// Supply a title.
+	// +optional
+	title string,
+
+	// Supply a body.
+	// +optional
+	body string,
+
+	// Add labels by name
+	// +optional
+	// label []string,
+
+	// Add the issue to a milestone by name
+	// +optional
+	// milestone string,
+) (string, error) {
+	args := []string{"gh", "issue", "create", "-R", repo}
+
+	if assignee != "" {
+		args = append(args, "--assignee", assignee)
+	}
+	if title != "" {
+		args = append(args, "--title", title)
+	}
+	if body != "" {
+		args = append(args, "--body", body)
+	}
+
+	// if len(label) > 0 {
+	// 	args = append(args, "--label", strings.Join(label, ","))
+	// }
+
+	// if milestone != "" {
+	// 	args = append(args, "--milestone", milestone)
+	// }
+
+	return m.base().WithExec(args).Stdout(ctx)
+}
+
 // List issues in a GitHub repository.
 func (m *Github) IssueList(
 	ctx context.Context,
@@ -128,6 +177,38 @@ func (m *Github) IssueComment(
 		"-R", repo,
 		issue,
 		"--body", body,
+	}
+
+	return m.base().WithExec(args).Stdout(ctx)
+}
+
+// Close issue
+func (m *Github) IssueClose(
+	ctx context.Context,
+	// Select another repository using the [HOST/]OWNER/REPO format
+	repo string,
+	// The issue number or URL
+	issue string,
+
+	// Leave a closing comment
+	// +optional
+	comment string,
+
+	// Reason for closing: {completed|not planned}
+	reason string,
+	// +optional
+) (string, error) {
+	args := []string{
+		"gh", "issue", "close",
+		"-R", repo,
+		issue,
+	}
+
+	if comment != "" {
+		args = append(args, "--comment", comment)
+	}
+	if reason != "" {
+		args = append(args, "--reason", reason)
 	}
 
 	return m.base().WithExec(args).Stdout(ctx)
