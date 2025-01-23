71770e28061cafdbec9fc16e7cc1e1c673390bbb
Andrea Luzzardi <andrea@luzzardi.com>
2025-01-23 09:58:59 -0800

    agent example cleanup
    Signed-off-by: Andrea Luzzardi <andrea@luzzardi.com>

--- examples/agent/comment_reply.sh
+++ examples/agent/comment_reply.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-go run . "reply to incoming issues and comments. make it funny. do not respond to comments prefixed with 🤖 (and make sure to use that prefix for your own messages)" ../../modules/github ../../modules/trufflehog
+go run . "reply to incoming issues and comments. make it funny. do not respond to comments prefixed with 🤖 (and make sure to use that prefix for your own messages)" \
+	../../modules/github
